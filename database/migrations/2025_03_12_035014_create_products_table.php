8c6f4f6f6fa23a7a4d069a2aaf9c0fab87489da8
MasaidFairus <masaidfairustrimarsongko@gmail.com>
2025-03-12 13:28:26 +0700

    first commit

---
+++ database/migrations/2025_03_12_035014_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('cover');
+            $table->unsignedBigInteger('price');
+            $table->unsignedBigInteger('creator_id');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('path_file');
+            $table->text('about');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
